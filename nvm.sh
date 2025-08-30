78b4c25096ca67cb8515face1c20cf0c31ce7310
pulagam344 <discordhari2004@gmail.com>
2025-08-30 23:01:43 +0530

    Create nvm.sh

---
+++ nvm.sh
@@ -0,0 +1,21 @@
+echo 4
+#!/bin/bash
+apt-get install -y sudo
+sudo apt-get update
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+source "$NVM_DIR/nvm.sh"
+nvm install 22
+nvm use 22
+nvm alias default 22
+npm install -g yarn
+!sudo npm install -g localtunnel
+
+pip install git+https://github.com/huggingface/trl.git@main
+pip install wandb==0.15.12
+pip install gensyn-genrl==0.1.6
+pip install reasoning-gym>=0.1.20 # for reasoning gym env
+pip install trl # for grpo config, will be deprecated soon
+pip install hivemind@git+https://github.com/gensyn-ai/hivemind@639c964a8019de63135a2594663b5bec8e5356dd # We need the latest, 1.1.11 is broken
+pip install --upgrade protobuf==6.31.1
